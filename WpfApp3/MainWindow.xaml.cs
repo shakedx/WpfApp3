a2f9d337ee1b19c7214238803a4e938c2f0b5b53
shakedx <kolesnikov_danil123@mail.ru>
2022-03-04 20:59:38 +0200

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1j(object sender, RoutedEventArgs e)
+        {
+            Result1.Text = new Linear().Action(double.Parse(a0j.Text), double.Parse(drj.Text), double.Parse(j.Text)).ToString();         
+        }
+
+        private void Button_Click2j(object sender, RoutedEventArgs e)
+        {
+            Result1.Text = new Exponential().Action(double.Parse(a0j.Text), double.Parse(drj.Text), double.Parse(j.Text)).ToString(); //mistake
+        }
+
+        private void Button_Click3sum(object sender, RoutedEventArgs e)
+        {
+            Result2.Text = new Linear().Sum(double.Parse(a0sum.Text), double.Parse(ansum.Text), double.Parse(nsum.Text)).ToString();         
+        }
+
+        private void Button_Click4sum(object sender, RoutedEventArgs e)
+        {
+            Result2.Text = new Exponential().Sum(double.Parse(a0sum.Text), double.Parse(ansum.Text), double.Parse(nsum.Text)).ToString();
+        }
+    }
+}
---
+++ WpfApp3/Series.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    internal abstract class Series
+    {
+        public abstract double Action(double a0,double step,double j);
+        public abstract double Sum(double a0,double an,double n);
+        public abstract string Print();
+    }
+    internal class Linear : Series /////////////////
+    {
+        public override double Sum(double a0, double an, double n)
+        {
+            return (n + 1) * (a0 + an);
+        }
+
+        public override string Print()
+        {
+            return " ";
+        }
+
+        public override double Action(double a0,double d,double j)
+        {
+            return a0 + j * d;                         
+        }
+    }
+    internal class Exponential : Series ///////////////////////////
+    {
+
+        public override double Sum(double a0, double an,double r)
+        {
+            return (a0 - an * r) / (1 - r);
+        }
+
+        public override string Print()
+        {
+            return " ";
+        }
+
+        public override double Action(double a0, double r,double j)
+        {
+            return a0*Math.Pow(r,j);
+        }
+    }
+}
